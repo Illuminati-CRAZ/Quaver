ea2cbb57a498c8332cb86c7c8e14d33bdcb5d887
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2021-06-17 22:25:26 -0700

    add config setting to allow use of external quaver db

--- Quaver.Shared/Database/DatabaseManager.cs
+++ Quaver.Shared/Database/DatabaseManager.cs
@@ -6,16 +6,11 @@ namespace Quaver.Shared.Database
     public class DatabaseManager
     {
         /// <summary>
-        ///     The path of the local database
-        /// </summary>
-        public static readonly string DatabasePath = ConfigManager.GameDirectory + "/quaver.db";
-
-        /// <summary>
         /// </summary>
         public static SQLiteConnection Connection { get; private set; }
 
         /// <summary>
         /// </summary>
-        public static void Initialize() => Connection = new SQLiteConnection(DatabasePath);
+        public static void Initialize() => Connection = new SQLiteConnection(ConfigManager.DatabasePath.Value);
     }
 }
